f8a1321fed83f2765733e07ce28201958e43146d
Abdullah <143931418+TypeAbdullah@users.noreply.github.com>
2025-05-30 22:28:37 +0600

    Update configs.py

--- configs.py
+++ configs.py
@@ -1,12 +1,12 @@
 from os import path, getenv
 
 class Config:
-    API_ID = int(getenv("API_ID", "28429143"))
-    API_HASH = getenv("API_HASH", "15afc60d067490eee8d34112487ac829")
-    BOT_TOKEN = getenv("BOT_TOKEN", "8018191301:AAEaM_kygBBd0vAcA4RR8RasqUhCNbYnWpk")
+    API_ID = int(getenv("API_ID", "27382214"))
+    API_HASH = getenv("API_HASH", "6a3913eb3f026ab02e7ac1c420df2ad0")
+    BOT_TOKEN = getenv("BOT_TOKEN", "8102248954:AAGpbzmUUQOmy3r_qkmik5r9C6lmwLaiRlQ")
     FSUB = getenv("FSUB", "Cultured_Mayhem")
-    CHID = int(getenv("CHID", "-1002402827530"))
-    SUDO = list(map(int, getenv("SUDO", "1685470205").split()))
+    CHID = int(getenv("CHID", "-1002100615083"))
+    SUDO = list(map(int, getenv("SUDO", "5984303934").split()))
     MONGO_URI = getenv("MONGO_URI", "mongodb+srv://Approve:Approve@cluster0.qavyu8x.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
     PORT = getenv("PORT", "8080")
 
